2d734283d703af6db16834543196ac024e414e0e
Valentin Mendez <valentinmendez0042@gmail.com>
2024-04-09 18:19:11 -0300

    imagenes

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded",function(){
+let currentSlide =0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index){
+    slides.forEach((slide, i)=>{
+        if(i == index){
+            slide.style.display = "block";
+        }else{
+            slide.style.display= "none";
+        }
+    });
+}
+ function nextSlide(){
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+ }
+ setInterval(nextSlide,3000)
+});
