b4caf54846be3300e5dc59af67c81cc9c4b8a890
Lucho <Lucianoceles@gmail.com>
2024-04-10 13:21:51 -0300

    imagenes

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var sliders = document.querySelectorAll('.slider');
-    sliders.forEach(function(slider){
-        var sl = slider.querySelectorAll('img'); // obtiene las imagenes de las clases sliders
-        var i=0; //indice de la imagen
-        function mostrarImagen(index){
-            sl.forEach(function(slide){
-                slide.style.display = 'none';
-            });
-            sl[i].style.display = 'block';
-        }
-        function cambiarAnterior(){
-            i = (i-1+sl.length)%sl.length;
-            mostrarImagen(i);
-        }
-        function cambiarSiguiente(){
-            i = (i+1)%sl.length;
-            mostrarImagen(i);
-        }
-        sl.addEventListener('click',function(event){
-            var clicX = event.clientX; //obtengo la posición X del click
-            var screenWidth = window.innerWidth; // obtengo el ancho de la pantalla
-            var centroPantalla = screenWidth / 2; // el centro de la pantalla
-            if(clicX<centroPantalla){
-                cambiarAnterior();
+    let currentSlide =0;
+    const slides = document.querySelectorAll(".slide");
+    
+    function showSlide(index){
+        slides.forEach((slide, i)=>{
+            if(i == index){
+                slide.style.display = "block";
             }else{
-                cambiarSiguiente();
+                slide.style.display= "none";
             }
         });
-        mostrarImagen(i);
-    });
+    }
+     function nextSlide(){
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+     }
+     setInterval(nextSlide,3000)
 });
